faeb366f2d75d19be852aa41d64d35e53d90f068
Eduardo Oliverio <dudu_oliverio@hotmail.com>
2018-12-02 21:04:51 -0200

    SplashXML

--- app/src/main/java/com/ti/luanbatista/projetomds/SplashActivity.java
+++ app/src/main/java/com/ti/luanbatista/projetomds/SplashActivity.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 
 public class SplashActivity extends Activity {
     // Timer da splash screen
-    private static int SPLASH_TIME_OUT = 1500;
+    private static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
